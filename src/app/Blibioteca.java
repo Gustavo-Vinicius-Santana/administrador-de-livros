83aaef2439d56fbb5fe1269a271ffdf5c96be1c0
ThalesHenri <thales1337@hotmail.com>
2024-05-23 09:21:47 -0300

    Coloquei a Biblioteca na pasta app, e apaguei a antiga na pasta models

--- src/app/Blibioteca.java
+++
@@ -1,9 +0,0 @@
-package app;
-import models.Livros;
-import  models.Usuarios;
-
-public class Blibioteca {
-    public static void main(String[] args){
-        System.out.print("Classe de blibiotecas.");
-    }
-}
